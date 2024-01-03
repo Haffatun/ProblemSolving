78bd118021c0b76f2f946a3451d3d122b076871b
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-01-03 22:57:48 +0600

    Create Codechef Round.c

---
+++ Codechef Round.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main ()
+{
+    int n;
+    scanf ("%d", &n);
+
+    switch (n)
+    {
+    case 1 :
+        printf ("NO\n");
+        break;
+    case 2 :
+        printf ("NO\n");
+        break;
+    case 3 :
+        printf ("NO\n");
+        break;
+    case 4 :
+        printf ("YES\n");
+        break;
+    case 5 :
+        printf ("NO\n");
+        break;
+    case 6 :
+        printf ("NO\n");
+        break;
+    case 7 :
+        printf ("NO\n");
+        break;
+
+    }
+}
+
+
