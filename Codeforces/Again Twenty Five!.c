654ae2caa562ab9693e0ad163624df4051acf7f5
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-01-26 13:51:52 +0600

    Create Again Twenty Five!.c

---
+++ Codeforces/Again Twenty Five!.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    long long n, i, r;
+    int a = 5;
+    scanf ("%lld", &n);
+    for (i = 0; i < n; i++)
+    {
+        r = a * a;
+    }
+    printf ("%lld\n", r);
+    return 0;
+}
