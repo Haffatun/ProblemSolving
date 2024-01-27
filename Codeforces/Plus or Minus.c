ba7d5d0d7f4c04cbd3801f5d3c10b02f47d5ab4b
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-01-27 13:05:07 +0600

    Create Plus or Minus.c

---
+++ Codeforces/Plus or Minus.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main() {
+    int a, b, c, t, i;
+    scanf ("%d", &t);
+    
+    for (i = 0; i < t; i++)
+    {
+        scanf ("%d%d%d", &a, &b, &c);
+        
+        if (a+b == c)
+        {
+            printf ("+\n");
+        }
+        else
+        {
+            printf ("-\n");
+        }
+    }
+ 
+    return 0;
+}
