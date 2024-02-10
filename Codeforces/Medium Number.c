9e1c3b2dcadb74dad96770f4cda3c5ea62181fb1
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-02-10 22:42:16 +0600

    Create Medium Number.c

---
+++ Codeforces/Medium Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a, b, c, t;
+  scanf("%d", &t);
+  while(t--)
+  {
+    scanf("%d%d%d", &a, &b, &c);
+    if((a<=b && b<=c) || (c<=b && b<=a))
+      printf("%d\n", b);
+    else if((b<=c && c<=a) || (a<=c && c<=b))
+      printf("%d\n", c);
+    else if((b<=a && a<=c) || (c<=a && a<=b))
+      printf("%d\n", a);
+  }
+}
