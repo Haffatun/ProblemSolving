7f277fcef662ab892e016a2f7df2150e412b0804
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-07-05 01:52:36 +0600

    Create Maximum Multiple Sum.cpp

---
+++ Codeforces/Codeforces Round 952 (Div. 4)/Maximum Multiple Sum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define lets_start    int main()
+#define okeyBye       return 0;
+#define ll            long long
+#define p             printf
+#define usi           unsigned int
+#define vi            vector<int>
+#define q             queue<int>
+ 
+lets_start
+{
+    int k; cin >> k;
+    while(k--)
+    {
+        int n; cin >> n;
+        if(n==3)
+            cout << n << endl;
+        else
+            cout << "2\n";
+    }
+ 
+    okeyBye
+}
